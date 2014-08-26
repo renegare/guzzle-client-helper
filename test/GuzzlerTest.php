e417f9b9e8377d4675420276c0c31d99e7621a0d
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-08-26 13:17:07 +0100

    test the untested

--- test/GuzzlerTest.php
+++ test/GuzzlerTest.php
@@ -3,9 +3,21 @@
 namespace Renegare\HTTP\Test;
 
 class GuzzlerTest extends \PHPUnit_Framework_TestCase {
+    use \Renegare\HTTP\GuzzlerTestTrait;
 
     public function testGuzzler() {
         $client = $this->getMockForAbstractClass('Renegare\HTTP\AbstractClient', ['http://api.example.com', $this->getMock('Psr\Log\LoggerInterface')]);
         $this->assertInstanceOf('GuzzleHttp\Client', $client->getGuzzle());
+    }
+
+    public function testGuzzlerTestTraitAssertCalled() {
+        $client = $this->getMockForAbstractClass('Renegare\HTTP\AbstractClient', ['http://api.example.com', $this->getMock('Psr\Log\LoggerInterface')]);
+        $this->mockHTTPResponse($client, 'POST', 'http://api.example.com/resource', function(){
+            return ['response' => '...'];
+        });
+
+        $response = $client->getGuzzle()->post('http://api.example.com/resource');
+        $this->assertInstanceOf('GuzzleHttp\Message\ResponseInterface', $response);
+        $this->assertEquals(['response' => '...'], $response->json());
     }
 }
